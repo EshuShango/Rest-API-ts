1b22f648257bf08be362f4595fe3eed094ed8986
EshuShango <tobiakinyemi21@gmail.com>
2023-06-05 15:56:16 -0500

    first init

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { getUserByEmail } from "../db/users";
+import {authentication, random} from "../helpers/index";
+import {createUser} from "../db/users";
+
+export const register =async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        password: authentication(password, salt),
+        salt,
+      },
+    });
+    return res.status(200).json(user).end();
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400)
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose  from "mongoose";
+import router from "./router";
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors({
+  credentials: true,
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}/`);
+});
+
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://olutakinyemi:hnY7Fyx9ABjlhM8g@cluster0.nmmzpvb.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+// mongoose.connection.on("error", (error: Error) => {
+//   console.log("MongoDB error", error);
+//   process.exit(1);
+// });
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
